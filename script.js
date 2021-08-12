c412fe2b6c614e8439a44f344b58ed0a171e7a38
Ogya Adyatma Putra <41492762+Adytm404@users.noreply.github.com>
2021-08-13 01:18:52 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('search-button').onclick = function() {
+    window.location = "https://ogyaadytm.tech/tools/ip-lookup/?id=" + document.getElementById('search-input').value;
+    return false;
+      }
